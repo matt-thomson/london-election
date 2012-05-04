2edb2a77533d5af9aa827b0455eb974cd805f026
Matt Thomson <matt.thomson@cantab.net>
2012-05-04 14:18:20 +0100

    Tidy up.

--- election.py
+++ election.py
@@ -19,21 +19,21 @@ while 1:
   if updated_elt:
     updated = updated_elt.text
 
-  if updated != prev_updated:
-    print updated
-    print "        Name         | Vote"
-    print "---------------------+-----"
+    if updated != prev_updated:
+      print updated
+      print "        Name         | Vote"
+      print "---------------------+-----"
 
-    table = soup.find("table", {"class" : "candidates"})
+      table = soup.find("table", {"class" : "candidates"})
 
-    for row in table.tbody.findAll("tr"):
-      name = row.find("td", {"class" : "candidate"}).contents[0]
-      result = row.find("td", {"class" : "result"}).text
+      for row in table.tbody.findAll("tr"):
+        name = row.find("td", {"class" : "candidate"}).contents[0]
+        result = row.find("td", {"class" : "result"}).text
 
-      print "%s | %s" % (name.ljust(20), result)
+        print "%s | %s" % (name.ljust(20), result)
 
-    print "\n"
+      print "\n"
     
-  prev_updated = updated
+    prev_updated = updated
 
   sleep(60)
