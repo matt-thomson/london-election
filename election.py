150c5b711a3c95cf916b531cf7f4b1d18348325e
Matt Thomson <matt.thomson@cantab.net>
2012-05-04 12:40:38 +0100

    Fix crass bug.

--- election.py
+++ election.py
@@ -3,14 +3,15 @@ from BeautifulSoup import BeautifulSoup
 from time import sleep
 
 url = "http://www.londonelects.org.uk/im-voter/results-and-past-elections/live-results-2012?contest=23"
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page)
 
 prev_updated = ""
 
 while 1:
+  page = urllib2.urlopen(url)
+  soup = BeautifulSoup(page)
+
   updated = soup.find("p", {"class" : "updated"}).text
-  
+
   if updated != prev_updated:
     print updated
 
@@ -19,11 +20,11 @@ while 1:
     for row in table.tbody.findAll("tr"):
       name = row.find("td", {"class" : "candidate"}).contents[0]
       result = row.find("td", {"class" : "result"}).text
-  
+
       print "%s : %s" % (name, result)
-  
+
     print "\n"
-	
+    
   prev_updated = updated
-  
+
   sleep(60)
